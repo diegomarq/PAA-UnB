5a8b076638e9e2d811d0d264e88e4e7c278d9b74
diegomarq <diegomarques.azevedo@gmail.com>
2016-04-19 21:43:55 -0300

    Conta inversao and update of mergeinsert

--- trab2/src/conta_inversao.c
+++ trab2/src/conta_inversao.c
@@ -1,43 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
 
-#define SIZE_S 100
-#define SIZE_C 2
+#define SIZE_S 6
 
-char data[SIZE_C][SIZE_S];
 int size = 0;
+long long int inversions = 0;
+
+typedef struct _string_array {
+  char s[6];
+}string_array;
+string_array* data;
 
 void insertionsort(int size) {
     char now[SIZE_S];
     int i, j;
 
     for (i = 1; i < size; i++) {
-        strcpy(now, data[i]);
+        strcpy(now, data[i].s);
         j = i-1;
-        while(j >= 0 && strcmp(now, data[j]) < 0) {
-		printf("(");
-		printf("%d", i);
-		printf(", ");
-		printf("%d", j);
-		printf(")\n");
-            strcpy(data[j+1], data[j]);
+        while(j >= 0 && strcmp(now, data[j].s) < 0) {
+            inversions++;
+            strcpy(data[j+1].s, data[j].s);
             j--;
         }
-        strcpy(data[j+1], now);
+        strcpy(data[j+1].s, now);
     }
 }
 
 int main(void) {  
     int i;
-    for(i =0; scanf("%[^\n]%*c", data[i]) != EOF; i++);  
+    // struct timeval start,end;
+    data = (string_array*)malloc(3300000*sizeof(string_array));
+    for(i =0; scanf("%[^\n]%*c", data[i].s) != EOF; i++);
     size = i;
 
+    // gettimeofday(&start,NULL);
     insertionsort(size);
+    printf("%lld\n", inversions);
+    // gettimeofday(&end,NULL);
+    // double delta = ((end.tv_sec  - start.tv_sec) * 1000000u + end.tv_usec - start.tv_usec) / 1.e6;
+    // printf("%lf\n",delta);
 
-//    for(i = 0; i < size; i++) {
-//        printf("%s", data[i]);
-//        printf("\n");
-//    }
+   // for(i = 0; i < size; i++) {
+   //     printf("%s", data[i].s);
+   //     printf("\n");
+   // }
     return 0;
 
   
--- trab2/src/mergeinsert.c
+++ trab2/src/mergeinsert.c
@@ -1,46 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
 
-#define SIZE_S 100 // string size
-#define SIZE_C 10 // number of elements
+#define SIZE_S 6
 
-char data[SIZE_C][SIZE_S];
 int size = 0;
+
+typedef struct _string_array {
+  char s[6];
+}string_array;
+string_array* data;
+string_array* aux;
 
 void insertionsort(int begin, int end) {
     char now[SIZE_S];
     int i, j;
 
     for (i = begin+1; i < end; i++) {
-        strcpy(now, data[i]);
+        strcpy(now, data[i].s);
         j = i-1;
-        while(j >= begin && strcmp(now, data[j]) < 0) {
-            strcpy(data[j+1], data[j]);
+        while(j >= begin && strcmp(now, data[j].s) < 0) {
+            strcpy(data[j+1].s, data[j].s);
             j--;
         }
-        strcpy(data[j+1], now);
+        strcpy(data[j+1].s, now);
     }
 }
 
-void merge(int begin, int middle, int end) {
-    char aux[SIZE_C][SIZE_S];
+void merge(int begin, int middle, int end) {    
     int i, j, k;
   
     for (i = begin; i < middle+1; i++)
-        strcpy(aux[i], data[i]);
+        strcpy(aux[i].s, data[i].s);
   
     for (j = middle+1; j < end+1; j++)
-        strcpy(aux[end+middle+1-j], data[j]);
+        strcpy(aux[end+middle+1-j].s, data[j].s);
 
     i = begin;
     j = end;
 
     for (k = begin; k < end+1; k++) {
-        if (strcmp(aux[i], aux[j]) < 0) {
-            strcpy(data[k], aux[i]);
+        if (strcmp(aux[i].s, aux[j].s) < 0) {
+            strcpy(data[k].s, aux[i].s);
             i++;
         } else {
-            strcpy(data[k], aux[j]);
+            strcpy(data[k].s, aux[j].s);
             j--;
         }
     }
@@ -64,20 +69,26 @@ void mergesort(int begin, int end, int k) {
 
 int main(int argc, char *argv[]) {  
     int i, k;
-
+    struct timeval start,end;  
+    data = (string_array*)malloc(3300000*sizeof(string_array));
+    aux = (string_array*)malloc(3300000*sizeof(string_array));
     if(argc == 1){
         printf("Usage : <program_name> <delimiter>\n");
         return 1;
     }
     k = atoi(argv[1]);
 
-    for(i =0; scanf("%[^\n]%*c", data[i]) != EOF; i++);  
+    for(i =0; scanf("%[^\n]%*c", data[i].s) != EOF; i++);  
     size = i;
-
+    
+    gettimeofday(&start,NULL);
     mergesort(0, size-1, k);
+    gettimeofday(&end,NULL);
+    double delta = ((end.tv_sec  - start.tv_sec) * 1000000u + end.tv_usec - start.tv_usec) / 1.e6;
+    printf("%lf\n",delta);
 
     for(i = 0; i < size; i++) {
-        printf("%s", data[i]);
+        printf("%s", data[i].s);
         printf("\n");
     }
     return 0;  
